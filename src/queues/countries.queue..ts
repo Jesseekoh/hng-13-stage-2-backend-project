91661d672169371dff0fffb37ef1bee6b5a1422b
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-10-26 18:46:08 +0100

    Delete countries queue file and update tsconfig

--- src/queues/countries.queue..ts
+++
@@ -1,8 +0,0 @@
-import Bull from "bull";
-
-// const countriesQueue = new Bull('countries', {
-//   redis: {
-//     host: process.env.REDIS_HOST,
-//     port: parseInt(process.env.REDIS_PORT),
-//   },
-// });
