c21ed058dbe1f44fc6806955801221e1af268032
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-10-26 17:33:09 +0100

    Create countries.types.ts

---
+++ src/queues/countries.queue..ts
@@ -0,0 +1,8 @@
+import Bull from "bull";
+
+// const countriesQueue = new Bull('countries', {
+//   redis: {
+//     host: process.env.REDIS_HOST,
+//     port: parseInt(process.env.REDIS_PORT),
+//   },
+// });
